15dc7945581821e64e9d0feda617c164a7eba2d8
Carlos Buitrago <letscatchupagain@gmail.com>
2025-03-09 15:15:13 +0000

    Install jest and supertest to run test on api.

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ import expensesRoutes from './routes/expensesRoutes.js';
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
